d671902ab6c7a5e5b2b929d4316ee728bc2af6ac
Erickkirui <erickkirui653@gmail.com>
2024-04-18 12:04:57 +0300

    updated docker file

--- Backend/run.py
+++ Backend/run.py
@@ -1,8 +1,8 @@
 from app import create_app
 from config import app_config
 
-config_name = "development"
+config_name = "production"
 app = create_app(app_config)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True , port=5000)
